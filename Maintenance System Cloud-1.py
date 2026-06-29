9b0a593570b566e29390263a1ff3008d008736b0
Somsak <kayppc@gmail.com>
2026-06-29 17:02:07 +0700

    Update Maintenance System Cloud-1.py

--- Maintenance System Cloud-1.py
+++ Maintenance System Cloud-1.py
@@ -375,9 +375,7 @@ else: m_type_selected = "CNC"
 # 🔧 [โหมดที่ 1: ฝั่งช่างเทคนิคส่งฟอร์มประจำวัน]
 # ==========================================
 if user_role == "🔧 ช่างเทคนิค (ส่งฟอร์ม)":
-    col_logo1, col_logo2, col_logo3 = st.columns([1, 2, 1])
-    with col_logo2:
-        st.image("Logo_Pes.png", width=140)
+    st.image("Logo_Pes.png", width=240)
 
     st.caption("PHOLLAWAT ENGINEERING SUPPLY CO., LTD.")
     st.title(f"📋 ใบตรวจสอบเครื่อง {machine_id} ประจำวัน")
